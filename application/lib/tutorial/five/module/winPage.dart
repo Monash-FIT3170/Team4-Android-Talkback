da6bc868167bc58f6c33b71538078b6d39b71432
Zane Gomes <semogenaz@kittymail.com>
2023-09-01 23:10:21 +1000

    Added end on lesson challenge one. This challenge combines the notification and scrolling skills.

--- application/lib/tutorial/five/module/winPage.dart
+++ application/lib/tutorial/five/module/winPage.dart
@@ -2,29 +2,26 @@ import 'package:application/tutorial/five/module/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:application/tutorial/five/module/end_of_lesson2.dart';
 
-class Challenge extends StatefulWidget {
-  const Challenge({Key? key}) : super(key: key);
+class WinPage extends StatefulWidget {
+  const WinPage({Key? key}) : super(key: key);
 
   @override
-  State<Challenge> createState() => _Challenge();
+  State<WinPage> createState() => _WinPage();
 }
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-class _Challenge extends State<Challenge> with WidgetsBindingObserver {
-  String introSpeech =
-      """Welcome to the first challenge of lesson five. You will recieve a 
-      notification from Ricky. 
-      You will have to do the following:
+class _WinPage extends State<WinPage> with WidgetsBindingObserver {
 
-      Navigate to the notification drawer by draging both fingers from the 
-      top to the bottom of the screen.
-      Then swipe right till you hear Ricky's notification.
-      Return to the challenge and select the number from her notification.
+    String middleSpeech =
+      """Welcome back, now select the correct number form the following by
+      swiping right to go over the numbers and then double tapping to select it.
       """;
 
+    String wrongButton = """Wrong number, try again""";
 
   bool isIntro = true;
   bool isFeedbackSpeech1 = false;
@@ -45,7 +42,6 @@ class _Challenge extends State<Challenge> with WidgetsBindingObserver {
     // Initilizing tts engine
     _initTextToSpeech().then((value) {
       flutterTts = value;
-      _speakLines(flutterTts!, introSpeech);
     });
   }
 
@@ -115,36 +111,71 @@ class _Challenge extends State<Challenge> with WidgetsBindingObserver {
             // in the middle of the parent.
 
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
 
-                // All the elements of the page
-                children: [
-          if (isIntro)
-            // This speaks the intro
-            FutureBuilder<FlutterTts>(
+      // All the elements of the page
+      children: [
+        if (isIntro)
+          // This speaks the intro
+          FutureBuilder<FlutterTts>(
+              future: _initTextToSpeech(),
+              builder: (context, snapshot) {
+                // This reads instructions and checks for successful
+                // execution of said instructions.
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasData) {
+                    _speakLines(snapshot.data!, middleSpeech);
+                  }
+                  return Column(
+                    children: [
+                      const SizedBox(),
+                      Text(middleSpeech), // Display the speech content
+                      FutureBuilder<FlutterTts>(
                 future: _initTextToSpeech(),
                 builder: (context, snapshot) {
-                  // This reads instructions and checks for successful
-                  // execution of said instructions.
                   if (snapshot.connectionState == ConnectionState.done) {
                     if (snapshot.hasData) {
-                      _speakLines(snapshot.data!, introSpeech);
-                      LocalNotification.challengeOneNotification(
-                          title: "Ricky",
-                          body: "3170",
-                          flutterLocalNotificationsPlugin:
-                              flutterLocalNotificationsPlugin);
+                      _speakLines(snapshot.data!, middleSpeech);
                     }
                     return Column(
                       children: [
-                        const SizedBox(), //,
-                        Text(introSpeech), // Display the speech content
+                        const SizedBox(),
+                        //Text(middleSpeech), // Display the speech content
+                        ElevatedButton(
+                          onPressed: () {
+                            _speakLines(snapshot.data!, wrongButton);
+                          },
+                          child: const Text("4"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Challenge2()),
+                            );
+                          },
+                          child: const Text("3170"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            _speakLines(snapshot.data!, wrongButton);
+                          },
+                          child: const Text("2023"),
+                        )
                       ],
                     );
                   } else {
                     return const CircularProgressIndicator();
                   }
                 })
-        ])));
+                    ],
+                  );
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              }),
+      ],
+    )));
   }
 }
