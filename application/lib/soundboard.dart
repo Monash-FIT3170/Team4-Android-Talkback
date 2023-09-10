1418f11e9abc7c1f4b4f8d5ab310e6c63e068795
TerryMonash <131653924+TerryMonash@users.noreply.github.com>
2023-09-11 00:04:19 +1000

    Updated soundboard.dart with the audio playing method

--- application/lib/soundboard.dart
+++ application/lib/soundboard.dart
@@ -15,4 +15,37 @@ class Soundboard extends StatefulWidget {
   _SoundboardState createState() => _SoundboardState();
 }
 
+class _SoundboardState extends State<Soundboard> {
+  late AudioPlayer audioPlayer;
 
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer = AudioPlayer();
+  }
+
+  void playSound() async {
+    
+    await audioPlayer.play('assets/your_audio_file.mp3', isLocal: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Soundboard Minigame'),
+      ),
+      body: Center(
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(6, (index) {
+            return ElevatedButton(
+              onPressed: playSound,
+              child: Text('Button ${index + 1}'),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
