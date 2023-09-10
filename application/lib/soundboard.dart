320410de71d4c4e3d851c48c3db66a75d9324ed2
TerryMonash <131653924+TerryMonash@users.noreply.github.com>
2023-09-11 00:03:03 +1000

    Update soundboard.dart added stateless widget class

--- application/lib/soundboard.dart
+++ application/lib/soundboard.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Soundboard(),
+    );
+  }
+}
+
+class Soundboard extends StatefulWidget {
+  @override
+  _SoundboardState createState() => _SoundboardState();
+}
+
 
