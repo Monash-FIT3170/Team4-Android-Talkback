4c03282ac9a6d7b7d9a9b46ee5544136e2530ec0
byebrid <lexlmgallon@gmail.com>
2023-10-08 13:40:49 +1100

    Automate ChatGPT translation, translate Tut 4
    - 'languages.py' now has the option to automatically get ChatGPT
      translations for missing keys. Requires paid account. Place OpenAI API
      key in '.env' file, make python virtual environment and
      'pip install -r scripts/requirements.txt'.
    - If 'ASK_GPT' is set to False, then script still outputs prompt to
      copy-paste into ChatGPT. Else you first get asked if you want to
      translate with ChatGPT. If yes, then also asked if you want the JSON
      file to be updated with the returned translations.
    - Used this to translate tutorial 4 into all supported languages. It
      even seemed to use Spanish accent when locale was set to Spanish
      despite the code setting TTS to "en-US"? Not sure why, but it's good
      for us!
    - Fix bug in how 'languages.py' counted unique keys. Was meant to use
      sets but had used list instead.

--- application/lib/tutorial/four/tutorial_four.dart
+++ application/lib/tutorial/four/tutorial_four.dart
@@ -1,10 +1,12 @@
 import 'package:application/tutorial/four/module/volume_media_control.dart';
 import 'package:application/common/tutorial.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-const TutorialMenu tutorialFour =
-    TutorialMenu(title: 'Tutorial 4', moduleButtons: [
+TutorialMenu tutorialFour =
+    TutorialMenu(title: 'tutorial'.tr(args: ['4']), moduleButtons: [
   // TutorialMenuButton(title: 'Start and stop media', module: null),
   TutorialMenuButton(
-      title: 'Media volume control', module: MediaVolumeControlPage()),
+      title: 'media_volume_control'.tr(),
+      module: const MediaVolumeControlPage()),
   // TutorialMenuButton(title: 'Challenge', module: null)
 ]);
--- scripts/languages.py
+++ scripts/languages.py
@@ -4,6 +4,18 @@ languages more automated.
 """
 from pathlib import Path
 import json
+import os
+
+import dotenv
+import openai
+
+dotenv.load_dotenv()
+
+
+ASK_GPT = True
+TRANSLATIONS_DIR = Path("application/assets/translations")
+if ASK_GPT:
+    openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 LanguageCode = str
@@ -14,8 +26,9 @@ def _load_translation_dicts(translations_dir: Path) -> dict[LanguageCode, Transl
     """Loads all translation files into a dictionary of <language_code> -> dict"""
     # Map of language code -> translation dict
     translation_dicts = {}
-    
-    for file in translations_dir.iterdir():
+        
+    files = sorted(translations_dir.iterdir())
+    for file in files:
         if file.name.endswith(".json"):
             # Strip the '.json' at end
             language_code = file.name[:-5]
@@ -26,19 +39,18 @@ def _load_translation_dicts(translations_dir: Path) -> dict[LanguageCode, Transl
     return translation_dicts
 
 
-def _get_all_unique_keys(translation_dicts: dict[LanguageCode, TranslationDict]) -> list[str]:
+def _get_all_unique_keys(translation_dicts: dict[LanguageCode, TranslationDict]) -> set[str]:
     """Get all unique keys found across all translation dictionaries."""
-    unique_keys = []
+    unique_keys = set()
     for translation_dict in translation_dicts.values():
-        unique_keys.extend(translation_dict.keys())
+        unique_keys.update(set(translation_dict.keys()))
 
     return unique_keys
 
 
-def _find_missing_keys(translation_dict: TranslationDict, unique_keys: list[str]):
+def _find_missing_keys(translation_dict: TranslationDict, unique_keys: set[str]) -> set[str]:
     """Find any missing keys in 'd' based on global list of unique keys."""
     d_keys = set(translation_dict.keys())
-    unique_keys = set(unique_keys)
     return unique_keys.difference(d_keys)
 
 
@@ -69,22 +81,74 @@ def _generate_chatgpt_prompt(language_code: str, keys: list[str], translation_di
     return prompt
 
 
+def _get_chatgpt_response(prompt: str):
+    """Get only the actual text of ChatGPT's reponse to the given 'prompt'."""
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create( 
+        model="gpt-3.5-turbo", messages=messages 
+    )
+    message = response.choices[0].message.content
+    return message
+
+
+def _update_translation_file(language_code: str, translation_dict: TranslationDict, gpt_translation: str):
+    """Appends ChatGPT translation to existing translation file."""
+    output_file = TRANSLATIONS_DIR / f"{language_code}.json"
+    print(f"Updating {output_file}...")
+    new_dict = json.loads(gpt_translation)
+    translation_dict.update(new_dict)
+    with open(output_file, "w") as f:
+        json.dump(translation_dict, f, ensure_ascii=False, indent=4)
+
+
+def _get_user_response(question: str, options: list[str]) -> str:
+    """
+    Continuously prompt user for response to 'question' until they answer with
+    any of the given 'options'.
+    """
+    while True:
+        response = input(question)
+        if response not in options:
+            print(f"    Must provide response from {options}\n")
+        else:
+            return response
+
+
 if __name__ == "__main__":
-    translation_dicts = _load_translation_dicts(Path("application/assets/translations"))
+    translation_dicts = _load_translation_dicts(TRANSLATIONS_DIR)
     print(f"Found language codes: {list(translation_dicts.keys())}")
     unique_keys = _get_all_unique_keys(translation_dicts)
-    print(f"Found {len(unique_keys)} unique keys across all translation files")
+    print(f"Found {len(unique_keys)} unique keys across all translation files\n")
     
     any_language_missing_keys = False
     for language_code, translation_dict in translation_dicts.items():
+        print("="*64, f"\nLanguage code='{language_code}'\n", "="*64, sep="")
         missing_keys = _find_missing_keys(translation_dict=translation_dict, unique_keys=unique_keys)
         print(f"{language_code:<5} has {len(missing_keys):>2} missing keys")
         if len(missing_keys) > 0:
-            print("="*64, f"\nFOR CHATGPT! Language code='{language_code}'\n", "="*64, sep="")
-            print(_generate_chatgpt_prompt(language_code=language_code, keys=missing_keys, translation_dicts=translation_dicts))
-            print("\n\n")
             any_language_missing_keys = True
-
+            print(f"Missing keys: {missing_keys}")
+            prompt = _generate_chatgpt_prompt(language_code=language_code, keys=missing_keys, translation_dicts=translation_dicts)
+            
+            # In this case, user would manually copy-paste so they need to see the prompt
+            if ASK_GPT:
+                print()
+                want_gpt = _get_user_response(f"Do you want ChatGPT to translate these into '{language_code}' [y/n]?: ", options=["y", "n"])
+                if want_gpt == "y":
+                    gpt_translation = _get_chatgpt_response(prompt=prompt)
+                    
+                    print(gpt_translation)
+                    is_good_response = _get_user_response("Received response from ChatGPT. Do you want to write this to file (y/n)?: ", ["y", "n"])
+                    if is_good_response == "y":
+                        _update_translation_file(language_code=language_code, translation_dict=translation_dict, gpt_translation=gpt_translation)
+                    else:
+                        print("Not writing to file")
+                else:
+                    print("Not asking ChatGPT for translation")
+            else:
+                print(prompt)
+        
+        print("\n")
 
     if not any_language_missing_keys:
         print("="*64, "\nSUCCESS! All language files are consistent\n", "="*64, sep="")
